edb2c5867f6c77e38be2ed81de7b8c05748cc77e
Mark Packham <markpackham1@gmail.com>
2020-02-24 13:23:14 +0000

    finish off components https://youtu.be/XuFDcZABiDQ?t=956

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Header } from "./components/Header";
 import { LearnedFrom } from "./components/LearnedFrom";
 import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+
 import "./App.css";
 
 function App() {
@@ -11,6 +15,9 @@ function App() {
       <LearnedFrom />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
